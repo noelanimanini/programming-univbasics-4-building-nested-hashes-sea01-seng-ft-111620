c204052bc1f55ffe314b94f089642cccf691d2e4
crystal  villanueva <noelani.villanueva@gmail.com>
2020-11-02 06:07:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,9 +4,9 @@ end
 
 def monopoly_with_second_tier
   {:railroads => {
-    :pieces = {"4"},
-    :rent_in_dollars = {},
-    :names = {}
+    :pieces => {"4"},
+    :rent_in_dollars => {},
+    :names => {}
     }}
 
 end
